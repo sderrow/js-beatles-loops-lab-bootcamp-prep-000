1c3803db3e0efde935cd3d550a7a5f03b267d20c
Sean Derrow <sderrow@mit.edu>
2017-11-10 17:28:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments) {
   var myArray = []
   for(var i = 0; i < musicians.length; i++) {
-    myArray.push(musicians[i] + " plays " instruments[i])
+    myArray.push(musicians[i] + " plays " + instruments[i])
   }
   return myArray
 }
